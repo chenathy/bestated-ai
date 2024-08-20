846bfc2e96e21254b9651611f8967dc7f2711a1a
Chen <Yukai.Chen@charter.com>
2024-08-20 11:06:24 -0500

    Benefits for Employer Desktop

--- src/pages/Employers/index.js
+++ src/pages/Employers/index.js
@@ -3,10 +3,15 @@ import { useMediaQuery } from 'react-responsive';
 import HeroComponent from '../../components/Hero';
 
 import employersHero from '../../assets/images/employers-hero.jpg';
+
+import AttractIcon from '../../assets/icons/Attract.svg';
+import LoyaltyIcon from '../../assets/icons/Loyalty.svg';
+import ProductivityIcon from '../../assets/icons/Productivity.svg';
 import './Employers.css';
 
 
 const ForEmployers = () => {
+
     return (
         <div className='employers'>
             {/* Hero  */}
@@ -18,6 +23,56 @@ const ForEmployers = () => {
                 aboutBtn={false}
             />
 
+            {/* Benefits for Employers */}
+            <div className='benefits-container'>
+                <h2>Why consider employer-sponsored wills?</h2>
+
+                <div className='benefits'>
+
+                    <div className='benefit'>
+                        <img
+                            src={AttractIcon}
+                            alt='AttractIcon'
+                        />
+                        <div className='text'>
+                            <h4>Attract top talent</h4>
+                            <p>
+                                Leading companies must stay on the cutting edge of the benefits landscape. 
+                                Expand your reputation as a forward-thinking employer to attract and retain the highest-quality candidates.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='benefit'>
+                        <img
+                            src={LoyaltyIcon}
+                            alt='LoyaltyIcon'
+                        />
+                        <div className='text'>
+                            <h4>Employee loyalty</h4>
+                            <p>
+                                Demonstrate your company’s commitment to supporting your employees and their families, and increase retention rates. 
+                                This reduces the high cost of employee turnover.
+                            </p>
+                        </div>
+                    </div >
+
+                    <div className='benefit'>
+                        <img
+                            src={ProductivityIcon}
+                            alt='ProductivityIcon'
+                        />
+                        <div className='text'>
+                            <h4>Enhanced productivity</h4>
+                            <p>
+                                People who are less stressed in their personal lives can be more present when at work. 
+                                Boost productivity by adding to your employees’ sense of work-life balance.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
             
 
         </div>
